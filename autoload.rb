d2cf6932de401035492ddc63424aed48706971b0
Мар'ян Ребега <marikrebega@gmail.com>
2020-07-31 16:58:27 +0300

    Edited some files

--- autoload.rb
+++ autoload.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'i18n'
 require 'yaml'
 require_relative 'lib/codebreaker_marian/entities/game'
 require_relative 'lib/codebreaker_marian/entities/data_storage'
