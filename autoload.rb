4b1d823f6123ab0c8af53451750a591429b8c329
Мар'ян Ребега <marikrebega@gmail.com>
2020-07-31 16:41:25 +0300

    Fixed i18n error and remove console files

--- autoload.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'i18n'
-require 'yaml'
-require_relative 'lib/codebreaker_marian/entities/game'
-require_relative 'lib/codebreaker_marian/entities/data_storage'
-require_relative 'lib/codebreaker_marian/entities/processor'
-require_relative 'lib/codebreaker_marian/entities/statistics'
-require_relative 'lib/codebreaker_marian/version'
--- index.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'autoload'
-
-Codebreaker::Entities::Menu.new.game_menu
